621aff05d454b2d52efb984abd837df2141e0828
ChrisBarthol <Chris.Barthol@gmail.com>
2014-03-18 09:20:35 -0400

    Green test suite

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -100,7 +100,7 @@ describe "User pages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
-        it { should have_title(user.name) }
+        it { should have_title("New Task") }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
 
@@ -109,7 +109,7 @@ describe "User pages" do
         let(:user) { User.find_by(email: 'user@example.com') }
 
         it { should have_link('Sign out') }
-        it { should have_title(user.name) }
+        it { should have_title("New Task") }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
